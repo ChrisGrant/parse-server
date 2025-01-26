6da898f44f3efe3c47d0911d65b4745ab03994cf
Chris Grant <christopherscottgrant@gmail.com>
2025-01-26 20:45:05 +0000

    fix: Redis 4 does not reconnect after unhandled error in RedisPubSub.js
    Signed-off-by: Chris Grant <christopherscottgrant@gmail.com>

--- src/Adapters/PubSub/RedisPubSub.js
+++ src/Adapters/PubSub/RedisPubSub.js
@@ -2,12 +2,32 @@ import { createClient } from 'redis';
 
 function createPublisher({ redisURL, redisOptions = {} }): any {
   redisOptions.no_ready_check = true;
-  return createClient({ url: redisURL, ...redisOptions });
+  const client = createClient({ url: redisURL, ...redisOptions });
+   client.on('error', err => {
+    _logger.default.error('RedisCacheAdapter Publisher client error', {
+      error: err
+    });
+  });
+  client.on('connect', () => {});
+  client.on('reconnecting', () => {});
+  client.on('ready', () => {});
+  client.on('end', () => {});
+  return client;
 }
 
 function createSubscriber({ redisURL, redisOptions = {} }): any {
   redisOptions.no_ready_check = true;
-  return createClient({ url: redisURL, ...redisOptions });
+  const client = createClient({ url: redisURL, ...redisOptions });
+   client.on('error', err => {
+    _logger.default.error('RedisCacheAdapter Subscriber client error', {
+      error: err
+    });
+  });
+  client.on('connect', () => {});
+  client.on('reconnecting', () => {});
+  client.on('ready', () => {});
+  client.on('end', () => {});
+  return client;
 }
 
 const RedisPubSub = {
